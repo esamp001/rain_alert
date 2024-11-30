9d73a4196b2e992e7200e91f0537722a3403c4e9
sampaga <ellejunsampaga@gmail.com>
2024-11-30 12:29:20 +0800

    commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+from twilio.rest import Client
+
+#twilio
+account_sid = "ACce3003bc616e18fbca94dbbed565d2a4"
+auth_token = "9ed2a753c39ad7824039c48c2a890057"
+CLIENT = Client(account_sid, auth_token)
+
+#open weather api
+MY_KEY = "07caee0baee9414850fa0e793fb77d46"
+PH_LATITUDE = "12.879721"
+PH_LONGITUDE = "121.774017"
+
+parameters = {
+    "lat": PH_LATITUDE,
+    "long": PH_LONGITUDE,
+    "appid": MY_KEY,
+    "cnt": 4
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast?lat=12.879721&lon=121.774017&appid=07caee0baee9414850fa0e793fb77d46", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+
+# if data["list"][0]["weather"][0]['id'] < 700:
+#     print("Bring Umbrella")
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    message = CLIENT.messages.create(
+        body = "It will rain today, please bring your umbrella ☂️",
+        from_= "+17753209620",
+        to="+639472084004",
+    )
+
+    print(message.sid)
